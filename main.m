aafb2483c2b4b6f8f79889c62a0434f504b653fc
giuliapezzutti <74766522+giuliapezzutti@users.noreply.github.com>
2021-05-12 13:33:19 +0200

    signal saving

--- main.m
+++ main.m
@@ -4,37 +4,34 @@ clc
 
 %% Load il segnale EEG e qualche proprietà
 
-[signal, header] = sload('dataset/A01E.gdf');
+[signal, header] = sload('dataset/A01T.gdf');
 
-DUR = 7.5;      %Durata del segmento di interesse
+DUR = 7.5;      %Segment duration
 fs = 250;       %Sampling frequency
-N = DUR * fs;   %Numero di sample in ogni segmento di interesse
+N = DUR * fs;   %Number of samples in the segment
 
 sigElements = [header.EVENT.POS, header.EVENT.DUR, header.EVENT.TYP]; %Extract the segments' information
 
-c = zeros(288,1);
 k = 1;
-for i = 1:length(sigElements(:,1))
-    elements = sigElements(:,3);
-    if elements(i) == 769 || elements(i) == 770 || elements(i) == 771 || elements(i) == 772
-        c(k)=i;
+
+for i = 1:length(sigElements(:,1))-1
+    
+    c_pos = sigElements(:,1); %Current initial position
+    c_pos = c_pos(i);
+    
+    c_dur = sigElements(:,2); %Current duration
+    c_dur = c_dur(i);
+    
+    c_ele = sigElements(:,3); %Current segment type (i+1???)
+    c_ele = c_ele(i+1);
+    
+    if (c_ele == 769 || c_ele == 770) && header.ArtifactSelection(k)==0 %Check not defined artifact and check the label
+        
+        s = signal(c_pos:(c_pos+c_dur-1),1:22); %Extract the signal of interest
+        
+        path = "dataset/EEG/S1_"+k+".mat"; %Save the signal
+        save(path, 's');
         k = k+1;
+        
     end
 end
-
-% k = 0;
-% for i = 1:length(sigElements(:,1))
-%     c_dur = sigElements(:,2);
-%     c_dur = c_dur(i);
-%     
-%     if c_dur == 1875
-%         c_pos = sigElements(:,1);
-%         c_pos = c_pos(i);
-%         
-%         s = signal(c_pos:(c_pos+c_dur-1),1:22);
-%         
-%         path = "dataset/EEG/S1_"+k+".mat";
-%         save(path, 's');
-%         k = k+1;
-%     end
-% end   
