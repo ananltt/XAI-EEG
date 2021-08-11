c8c9ab1fa678f8318d28dac220198004559eeded
giuliapezzutti <74766522+giuliapezzutti@users.noreply.github.com>
2021-08-11 16:38:19 +0200

    Figure for report

--- source/variability_analysis.py
+++ source/variability_analysis.py
@@ -19,7 +19,7 @@ def extract_accuracies_matrix(path):
 
 
 def variability_analysis(output_folder):
-    print("\nVariability analysis!\n")
+    print("\nVariability analysis ...\n")
 
     path = output_folder + '/tot_accuracies.csv'
     accuracies = extract_accuracies_matrix(path)[0]
@@ -34,7 +34,7 @@ def variability_analysis(output_folder):
     path = output_folder + '/tot_right_accuracies.csv'
     accuracies = extract_accuracies_matrix(path)[0]
     baseline_right = np.mean(accuracies, axis=0)
-    print("Baseline left labels: ", baseline_right)
+    print("Baseline right labels: ", baseline_right)
 
     paths = glob(output_folder + '/zero_*.csv')
     paths = paths + glob(output_folder + '/interpolation_*.csv')
@@ -53,27 +53,47 @@ def variability_analysis(output_folder):
 
         if name.find('left') != -1:
             b = baseline_left
+            title = 'Test Left Hand - '
         elif name.find('right') != -1:
             b = baseline_right
+            title = 'Test Right Hand - '
         else:
             b = baseline_both
+            title = 'Test Both Hands - '
 
         difference = np.repeat(b, zero_accuracies.shape[0] * zero_accuracies.shape[1]).reshape(zero_accuracies.shape)
         difference = np.subtract(difference, zero_accuracies)
         difference = difference * 100 / b
         mean_difference = np.mean(difference, axis=0)
+        for i, val in enumerate(mean_difference):
+            mean_difference[i] = round(val, 3)
 
-        print(*mean_difference, sep=" & ")
+        print(*mean_difference, sep="\% & ")
+
+        dict_channels = {'C1': 'FZ', 'C2': 'FC3', 'C3': 'FC1', 'C4': 'FCz', 'C5': 'FC2',
+                         'C6': 'FC4', 'C7': 'C5', 'C8': 'C3', 'C9': 'C1', 'C10': 'CZ', 'C11': 'C2', 'C12': 'C4',
+                         'C13': 'C6', 'C14': 'CP3', 'C15': 'CP1', 'C16': 'CPz', 'C17': 'CP2', 'C18': 'CP4',
+                         'C19': 'P1', 'C20': 'PZ', 'C21': 'P2', 'C22': 'POz'}
 
         if name.find('channel') != -1:
             labels = ['C' + str(i + 1) for i in range(0, zero_accuracies.shape[1])]
+            labels = [dict_channels.get(l) for l in labels]
+            title = title + 'Channel '
         else:
-            labels = ['S' + str(i + 1) for i in range(0, zero_accuracies.shape[1])]
+            labels = ['s' + str(i + 1) for i in range(0, zero_accuracies.shape[1])]
+            title = title + 'Segment '
 
-        fig, axs = plt.subplots()
-        axs.set_title('Accuracy variability: {}'.format(name))
+        if name.find('permutation') != -1:
+            title = title + 'Permutation'
+        elif name.find('zero') != -1:
+            title = title + 'Zero-Ablation'
+        else:
+            title = title + 'Interpolation-Ablation'
+
+        fig, axs = plt.subplots(figsize=(12, 8))
+        axs.set_title(title)
         axs.boxplot(difference, labels=labels)
-        axs.set_ylim([-100, 100])
+        axs.set_ylim([-50, 100])
         plt.tight_layout()
         plt.savefig(output_folder + '/{}'.format(name))
         plt.show()
